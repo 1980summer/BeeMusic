e1853c3ab25604bebfc186a60aa20fbedfc8bac2
1980summer <lyuzuru@foxmail.com>
2022-05-13 10:22:34 +0800

    视频页面-数据展示

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -3,41 +3,20 @@ import { getTopMVs } from '../../service/api_video'
 // import beeRequest from '../../service/index'
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    topMVs: {}
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        topMVs: {}
+    },
 
-  /**
-   * 生命周期函数--监听页面加载(created)
-   */
-  onLoad: function (options) {
-    // const _this = this
-    getTopMVs(0).then(res => {
-      this.setData({ topMVs: res.data })
-      console.log(res.data)
-    })
-    // ===================================================================
-    // beeRequest.get("/top/mv", { offset: 0, limit: 10 }).then(res => {
-    //   this.setData({ topMVs: res.data })
-    //   console.log(res.data)
+    /**
+     * 生命周期函数--监听页面加载(created)
+     */
+    onLoad: async function (options) {
+        const res = await getTopMVs(0)
+        this.setData({ topMVs: res.data })
 
-    // })
-    // =================================================
-    // wx.request({
-    //     url: 'http://123.207.32.32:9001/top/mv',
-    //     data: {
-    //         offset: 0,
-    //         limit: 10
-    //     },
-    //     success: function (res) {
-    //         _this.setData({ topMVs: res.data.data })
-    //         console.log(res)
-    //     }
-    // })
-    // ======================================================
-  }
+    }
 })
 
