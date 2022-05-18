5c61a7efc5c486773657fc2aa8a4558fac654c0e
1980summer <lyuzuru@foxmail.com>
2022-05-18 20:36:50 +0800

    MV详情页-获取数据

--- pages/detail-video/index.js
+++ pages/detail-video/index.js
@@ -1,11 +1,14 @@
 // pages/detail-video/index.js
+import { getMVURL, getMVDetail, getRelatedVideo } from '../../service/api_video'
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        mvURLInfo: {},
+        mvDetail: {},
+        relatedVideos: []
     },
 
     /**
@@ -13,10 +16,21 @@ Page({
      */
     onLoad(options) {
         const id = options.id
-        console.log(id)
-        // 1 请求播放地址
-        // 2 请求视频信息
-        // 3 请求相关视频
-    },
 
+    },
+    // 
+    getPageData() {
+        // 1 请求播放地址
+        getMVURL(id).then(res => {
+            this.setData({ mvURLInfo: res.data })
+        })
+        // 2 请求视频信息
+        getMVDetail(id).then(res => {
+            this.setData({ mvDetail: res.data })
+        })
+        // 3 请求相关视频
+        getRelatedVideo(id).then(res => {
+            this.setData({ relatedVideos: res.data })
+        })
+    }
 })
--- service/api_video.js
+++ service/api_video.js
@@ -1,8 +1,38 @@
 import beeRequest from './index'
-
+// 请求MV的列表
 export function getTopMVs(offset, limit = 10) {
-  return beeRequest.get("/top/mv", {
-    offset,
-    limit
-  })
+    return beeRequest.get("/top/mv", {
+        offset,
+        limit
+    })
 }
+
+/**
+ * 请求MV的播放地址
+ * @param {number} id 此MV对应的id
+ */
+export function getMVURL(id) {
+    return beeRequest.get('/mv/url', {
+        id
+    })
+}
+
+/**
+ * 请求MV的详情
+ * @param {number} mvid 此MV对应的id
+ */
+export function getMVDetail(mvid) {
+    return beeRequest.get("/mv/detail", {
+        mvid
+    })
+}
+
+/**
+ * 请求此MV相关的MV
+ * @param {number}} id 此MV对应的id
+ */
+export function getRelatedVideo(id) {
+    return beeRequest.get("/related/allvideo", {
+        id
+    })
+}   
