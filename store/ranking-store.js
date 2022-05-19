d93361f9316bbbde74ea0b016df1aba0be71acbc
1980summer <lyuzuru@foxmail.com>
2022-05-19 23:03:46 +0800

    Music页面-排行榜数据的请求和处理

--- store/ranking-store.js
+++ store/ranking-store.js
@@ -1,15 +1,45 @@
+
 import { HYEventStore } from 'hy-event-store'
 import { getRanking } from '../service/api_music'
 
+const rankingMap = { 0: "newRanking", 1: "hotRanking", 2: "originRanking", 3: "upRanking" }
+
 const rankingStore = new HYEventStore({
     state: {
-        hotRanking: {}
+        newRanking: {}, // 0 新歌
+        hotRanking: {}, // 1：热歌
+        originRanking: {}, // 2：原创
+        upRanking: {}, // 3：飙升
     },
     actions: {
         getRankingDataAction(ctx) {
-            getRanking(1).then(res => {
-                ctx.hotRanking = res.playlist
-            })
+            for (let i = 0; i < 4; i++) {
+                getRanking(i).then(res => {
+                    const rankingName = rankingMap[i]
+                    ctx[rankingName] = res.playlist
+
+                    // switch (i) {
+                    //     case 0:
+                    //         // console.log("新歌榜", res)
+                    //         ctx.newRanking = res.playsist
+                    //         break;
+                    //     case 1:
+                    //         // console.log("热歌榜", res)
+                    //         ctx.hotRanking = res.playsist
+                    //         break;
+                    //     case 2:
+                    //         // console.log("原创榜", res)
+                    //         ctx.originRanking = res.playsist
+                    //         break;
+                    //     case 3:
+                    //         // console.log("飙升榜", res)
+                    //         ctx.upRanking = res.playsist
+                    //         break;
+
+                    // }
+                })
+            }
+
         }
     }
 })
