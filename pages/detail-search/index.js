47179e384a91bb1c263fd90ac9c39643c79514e5
1980summer <lyuzuru@foxmail.com>
2022-05-20 22:13:42 +0800

    搜索页面-使用富文本优化搜索建议展示

--- pages/detail-search/index.js
+++ pages/detail-search/index.js
@@ -10,7 +10,8 @@ Page({
     data: {
         hotKeyWords: [],
         suggestSongs: [],
-        searchValue: ""
+        searchValue: "",
+        suggestSongsNodes: []
     },
     onLoad(options) {
         // 1 获取页面的数据
@@ -40,7 +41,45 @@ Page({
 
         // 4 根据关键字进行搜索
         debounceGetSearchSuggest(searchValue).then(res => {
-            this.setData({ suggestSongs: res.result.allMatch })
+            // 1-获取建议的关键字歌曲
+            const suggestSongs = res.result.allMatch
+            this.setData({ suggestSongs })
+
+            // 2-转成nodes节点
+            const suggestKeyWords = suggestSongs.map(item => item.keyword)
+            const suggestSongsNodes = []
+            for (const keyword of suggestKeyWords) {
+                const nodes = []
+                // startsWith 判断某个数据是否以XXX开头(es6)
+                // 大小写都匹配
+                if (keyword.toUpperCase().startsWith(searchValue.toUpperCase())) {
+
+                    const key1 = keyword.slice(0, searchValue.length)
+                    const node1 = {
+                        name: "span",
+                        attrs: { style: "color: #26ce8a;" },
+                        children: [{ type: "text", text: key1 }]
+                    }
+                    nodes.push(node1)
+                    const key2 = keyword.slice(searchValue.length)
+                    const node2 = {
+                        name: "span",
+                        attrs: { style: "color: #000;" },
+                        children: [{ type: "text", text: key2 }]
+                    }
+                    nodes.push(node2)
+                } else {
+                    const node = {
+                        name: "span",
+
+                        attrs: { style: "color: #000;" },
+                        children: [{ type: "text", text: keyword }]
+                    }
+                    nodes.push(node)
+                }
+                suggestSongsNodes.push(nodes)
+            }
+            this.setData({ suggestSongsNodes })
         })
 
     }
