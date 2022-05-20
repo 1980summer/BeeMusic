6314744dcc2ccf8aae1f582202aacaaa3db64589
1980summer <lyuzuru@foxmail.com>
2022-05-20 20:34:12 +0800

    搜索页面-搜索建议的获取和展示

--- pages/detail-search/index.js
+++ pages/detail-search/index.js
@@ -1,19 +1,41 @@
 // pages/detail-search/index.js
-import { getSearchHot } from '../../service/api-search'
+import { getSearchHot, getSearchSuggest } from '../../service/api-search'
 Page({
 
     data: {
-        hotKeyWords: []
+        hotKeyWords: [],
+        suggestSongs: [],
+        searchValue: ""
     },
     onLoad(options) {
         // 1 获取页面的数据
         this.getPageData()
     },
 
-    // 封装一个函数
+    // 网络请求
     getPageData: function () {
         getSearchHot().then(res => {
             this.setData({ hotKeyWords: res.result.hots })
+        })
+
+    },
+    // 事件处理
+    handleSearchChange: function (event) {
+        // 1 获取输入的关键字
+        const searchValue = event.detail
+
+        // 2 保存关键字
+        this.setData({ searchValue })
+
+        // 3 判断关键字为空字符的处理逻辑
+        if (!searchValue.length) {
+            this.setData({ suggestSongs: [] })
+            return
+        }
+
+        // 4 根据关键字进行搜索
+        getSearchSuggest(searchValue).then(res => {
+            this.setData({ suggestSongs: res.result.allMatch })
         })
 
     }
--- service/api-search.js
+++ service/api-search.js
@@ -3,3 +3,10 @@ import beeRequest from './index'
 export function getSearchHot() {
     return beeRequest.get("/search/hot")
 }
+
+export function getSearchSuggest(keywords) {
+    return beeRequest.get("/search/suggest", {
+        keywords,
+        type: "mobile"
+    })
+}
