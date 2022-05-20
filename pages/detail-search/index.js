f2200347857b0ebfc0091e1fc9d5ec52231daa0a
1980summer <lyuzuru@foxmail.com>
2022-05-21 01:20:25 +0800

    搜索框显示优化

--- pages/detail-search/index.js
+++ pages/detail-search/index.js
@@ -38,6 +38,7 @@ Page({
         // 3 判断关键字为空字符的处理逻辑
         if (!searchValue.length) {
             this.setData({ suggestSongs: [] })
+            this.setData({ resultSongs: [] })
             return
         }
 
